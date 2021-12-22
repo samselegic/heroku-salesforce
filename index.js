db8f4c134531da1a7e9a916e73d558370421073b
samselegic <samrat@selegic.com>
2021-12-22 23:15:26 +0530

    fix env problem

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
-const port = "8000";
+const port = process.env.PORT || 8000;
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use("/", require("./routes"));
+
 //Server Listner
 app.listen(port, function (err) {
   if (err) {
